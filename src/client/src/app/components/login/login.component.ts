d305a0cdb809996b58af4458e8208a850e0a46b4
brycejjacks <brycejjacks@gmail.com>
2022-01-11 14:00:30 -0800

    feat(client-login): add ts base to login component

--- src/client/src/app/components/login/login.component.ts
+++ src/client/src/app/components/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { UserService } from 'src/app/services/user.service';
+// import { signIn } from 'src/app/store/actions/user/sign-in.action';
+// import { User } from '../../../../../shared/models/user.model';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
+export class SignInComponent implements OnInit {
+  signInForm: FormGroup
+  constructor(
+    private userService: UserService,
+    private store: Store,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.signInForm = this.fb.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required],
+    })
+   }
 
   ngOnInit(): void {
   }
 
+  signIn() {
+    console.log(this.signInForm.value)
+    //this.userService.signIn(this.signInForm.value).subscribe();
+    //this.store.dispatch(signIn(this.signInForm.value));
+    //this.router.navigate(['home'])
+  }
 }
