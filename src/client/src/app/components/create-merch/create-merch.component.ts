1428ba3ec6df90857016d068d1c861ab7fd1c980
Aman <amandeepk221@gmail.com>
2022-01-12 20:08:57 -0800

    feat(client): create component create-merch

---
+++ src/client/src/app/components/create-merch/create-merch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-merch',
+  templateUrl: './create-merch.component.html',
+  styleUrls: ['./create-merch.component.scss']
+})
+export class CreateMerchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
