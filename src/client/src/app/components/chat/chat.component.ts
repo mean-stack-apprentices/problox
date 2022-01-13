ec797a4a5f6ea38f794acf7783bd3c40dda695cb
saba <sabera@alphaworks.tech>
2022-01-12 16:39:18 -0800

    feat(component): add chat functions

--- src/client/src/app/components/chat/chat.component.ts
+++ src/client/src/app/components/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/services/chat.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-chat',
@@ -10,7 +12,40 @@ export class ChatComponent implements OnInit {
   messageText = '';
   room='';
   messageArray: Array<{user: String , message: String }> = [];
-  constructor() { }
+
+  constructor
+  (private chatService: ChatService,
+    private userService:UserService
+    ) {
+    this.chatService.newUserJoined()
+    .subscribe((data: { user: String; message: String; }) => this.messageArray.push(data));
+
+    this.chatService.userLeftRoom()
+    .subscribe((data: { user: String; message: String; }) => this.messageArray.push(data));
+
+  this.chatService.newMessageRecevied()
+    .subscribe((data: { user: String; message: String; }) => this.messageArray.push(data));
+}
+
+join(){
+    this.chatService.joinRoom({user: this.user, room: this.room});
+}
+
+leave(){
+  this.chatService.leaveRoom({user: this.user, room: this.room});
+}
+
+sendMessage()
+{
+  this.chatService.sendMessage({ user: this.user, room: this.room, message: this.messageText });
+
+  this.chatService.createMessage({
+sender: this.user,
+  text: this.messageText,
+}).subscribe();
+
+
+   }
 
   ngOnInit(): void {
   }
