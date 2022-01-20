159faab6463daa826da849617282b36fc0e7d121
Gichelle Amon <amongichelle@gmail.com>
2022-01-20 09:35:49 -0800

    ref(ngrx): update game selector

--- src/client/src/app/store/selectors/game/game.selectors.ts
+++ src/client/src/app/store/selectors/game/game.selectors.ts
@@ -1,9 +1,8 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import {AppState} from '../..';
-// import * as fromGame from '../../reducers/game/game.reducer';
-import { State, gameFeatureKey } from '../../reducers/game/game.reducer'
+import * as fromGame from '../../reducers/game/game.reducer';
 
-const gameFeatureSelector = createFeatureSelector<AppState, State>(gameFeatureKey)
+const gameFeatureSelector = createFeatureSelector<AppState, fromGame.State>(fromGame.gameFeatureKey)
 
 export const gamesSelector = createSelector(
   gameFeatureSelector,
