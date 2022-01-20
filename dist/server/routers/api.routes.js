e327b2d078be38918c9595d28faad60b27c03a9a
Angel <angelcruz981@yahoo.com>
2022-01-19 17:25:23 -0800

    refactor(server): change router lines to work with front end

--- dist/server/routers/api.routes.js
+++ dist/server/routers/api.routes.js
@@ -1,36 +1,31 @@
-import express, { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import express from "express";
 import { userRouter } from "./user-route.js";
 import { gameRouter } from "./game-route.js";
 import { chatRouter } from "./chat-route.js";
-
 export const apiRouter = express.Router();
-
 // Routes go here
 apiRouter.use('', userRouter);
 apiRouter.use('', gameRouter);
 apiRouter.use('', chatRouter);
-
 // resspone handler 
 apiRouter.use((req, res, next) => {
     if (res.locals.data) {
-      res.status(200).json({ data: res.locals.data });
-    } else {
-      next();
+        res.status(200).json({ data: res.locals.data });
     }
-  })
-
-  // express error handling middleware
-apiRouter.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    else {
+        next();
+    }
+});
+// express error handling middleware
+apiRouter.use((err, req, res, next) => {
     if (err.name === 'ValidationError') {
-      res.status(400).send(err);
+        res.status(400).send(err);
     }
     console.log(Object.keys(err));
     res.status(500).send(err);
-  });
-  
-  
-  
-  apiRouter.all("/*", function (req, res) {
-    console.log('not found')
+});
+apiRouter.all("/*", function (req, res) {
+    console.log('not found');
     res.sendStatus(404);
-  });
+});
+//# sourceMappingURL=api.routes.js.map
