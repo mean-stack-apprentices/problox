d6ce035247aef34035b99a16d16698eccab6c6c7
Aman <amandeepk221@gmail.com>
2022-01-25 08:58:54 -0800

    feat(client): add route for merch-details component

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { PageJoinGameComponent } from './pages/page-join-game/page-join-game.component';
 import { CreateMerchComponent } from './components/create-merch/create-merch.component';
 import { MerchComponent } from './components/merch/merch.component';
+import { MerchDetailsComponent } from './components/merch-details/merch-details.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -19,6 +20,7 @@ const routes: Routes = [
   {path: 'games', loadChildren:
   () => import('./modules/games/module-games.module').then(m => m.GamesModule)
   },
+  {path: 'merch-details', component: MerchDetailsComponent},
   {path: '**', redirectTo: '/login'}
 ];
 
