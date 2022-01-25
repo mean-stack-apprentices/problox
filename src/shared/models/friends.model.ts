f77be667f1239a1f4b335ad5bbe7c33d1866d3d2
saba <sabera@alphaworks.tech>
2022-01-25 12:07:05 -0800

    ref(model): remove text property & add friends property

--- src/shared/models/friends.model.ts
+++ src/shared/models/friends.model.ts
@@ -1,8 +1,8 @@
-import type mongoose from 'mongoose';
+
 import type { User } from './user.model.js';
 export interface Friends {
-    _id?:string
-    text:string
-    user: {type: mongoose.Types.ObjectId} | User;
+    _id?:string;
+    user: User;
+    friends:[]
   }
   
