639c1c48ea0dc4b929a1795214d5c5e9a349a9d5
Angel <angelcruz981@yahoo.com>
2022-01-25 16:13:40 -0800

    feat(order): create order schema

---
+++ src/server/schemas/order.schema.ts
@@ -0,0 +1,41 @@
+import  mongoose  from "mongoose"
+import { Order } from "../../shared/models/order.model"
+const {Schema, model} = mongoose
+
+
+const orderSchema = new Schema<Order>({
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: true
+      }],
+      shippingAddress1: {
+          type: String,
+          required: true
+      },
+    city: {
+        type: String,
+        required: true
+    },
+    zip: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    totalPrice: {
+        type: Number
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }
+})
+
+exports.Order = mongoose.model('Order', orderSchema)
