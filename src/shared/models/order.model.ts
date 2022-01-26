69a55e446dcf18beb2ae7bb66e415bd37e5c7fce
Angel <angelcruz981@yahoo.com>
2022-01-26 10:16:29 -0800

    refactor(order): added shippingAddress interface

--- src/shared/models/order.model.ts
+++ src/shared/models/order.model.ts
@@ -1,14 +1,18 @@
 import { orderItems } from "./order-items.model";
 import { User } from "./user.model";
 
-export interface Order {
-    _id?: string,
-    orderItems: orderItems[],
-    shippingAddress1: string,
+interface shippingAddress {
+    street: string,
     city: string,
     zip: string,
     country: string,
-    phone: string,
+    phone: string
+}
+
+export interface Order {
+    _id?: string,
+    orderItems: orderItems[],
+    shippingAddress: shippingAddress,
     totalPrice: number,
     user: string | User
 }
