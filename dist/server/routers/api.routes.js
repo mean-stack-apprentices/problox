db3eafd07bff5ed3f99303116a3b646ee0664f33
Angel <angelcruz981@yahoo.com>
2022-01-31 11:09:01 -0800

    not sure where this came from

--- dist/server/routers/api.routes.js
+++ dist/server/routers/api.routes.js
@@ -5,6 +5,7 @@ import { chatRouter } from "./chat-route.js";
 import { merchRouter } from "./merch-route.js";
 import { adminRouter } from "./admin.route.js";
 import { emailRouter } from "./email-route.js";
+import { orderRouter } from "./order.route.js";
 export const apiRouter = express.Router();
 // Routes go here
 apiRouter.use('/users', userRouter);
@@ -13,6 +14,7 @@ apiRouter.use('/chat', chatRouter);
 apiRouter.use('/merch', merchRouter);
 apiRouter.use('/admin', adminRouter);
 apiRouter.use('/email', emailRouter);
+apiRouter.use('/order', orderRouter);
 // response handler 
 apiRouter.use((req, res, next) => {
     if (res.locals.data) {
