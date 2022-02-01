1e7ead34d4a081a218ab08dafb3052edb2d977c3
Lee <tab271621@gmail.com>
2022-01-31 20:29:49 -0600

    ref(server):change role to roles in user schema

--- src/server/schemas/user.schema.ts
+++ src/server/schemas/user.schema.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import type { User } from '../../shared/models/user.model';
+import type { User } from '../../shared/models/user.model.js';
 const {Schema, model} = mongoose
 
 const userSchema = new Schema<User>({
@@ -7,7 +7,7 @@ const userSchema = new Schema<User>({
     username: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
-    role: {type: String, required: true},
+    roles: [{type: mongoose.Types.ObjectId, required: true, ref: 'Role'}],
 });
 
 export const UserModel = model<User>('User',userSchema)
