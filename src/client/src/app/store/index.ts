82010303d02f26801622840502c72dd432a94496
Gichelle Amon <amongichelle@gmail.com>
2022-02-01 13:46:14 -0800

    chore(store): remove game reducer

--- src/client/src/app/store/index.ts
+++ src/client/src/app/store/index.ts
@@ -7,21 +7,18 @@ import {
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import * as fromUser from './reducers/user/user.reducer';
-import * as fromGame from '../modules/games/store/game.reducer';
 import * as fromMerch from './reducers/merch/merch.reducer';
 
 
 export interface AppState {
 
   [fromUser.userFeatureKey]: fromUser.State;
-  [fromGame.gameFeatureKey]: fromGame.State;
   [fromMerch.merchFeatureKey]: fromMerch.State;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
 
   [fromUser.userFeatureKey]: fromUser.reducer,
-  [fromGame.gameFeatureKey]: fromGame.reducer,
   [fromMerch.merchFeatureKey]: fromMerch.reducer,
 };
 
