cfa2c70739f9406746cbda18346626f15a1e47d4
Lee <tab271621@gmail.com>
2022-02-02 17:36:08 -0600

    ref(client):add role insideapp-routing for create-merch

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { PageJoinGameComponent } from './pages/page-join-game/page-join-game.com
 import { CreateMerchComponent } from './components/create-merch/create-merch.component';
 import { MerchComponent } from './components/merch/merch.component';
 import { MerchDetailsComponent } from './components/merch-details/merch-details.component';
+import { RolesGuard } from './guards/roles.guard';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -16,7 +17,11 @@ const routes: Routes = [
   {path: 'create-message', component:ChatComponent},
   {path:'contact-page', component:ContactPageComponent},
   {path: 'create-merch', component: CreateMerchComponent},
-  {path: 'merch', component: MerchComponent},
+
+  {path: 'create-merch', component: CreateMerchComponent,
+  canActivate: [RolesGuard], data:{roles:["ADMIN"]}},
+
+
   {path: 'merch-details', component: MerchDetailsComponent},
   {path: 'games', loadChildren:
   () => import('./modules/games/module-games.module').then(m => m.GamesModule)
