54244f1b4cb4f822f4ac0e1fe665950d2e7474a9
Lee <tab271621@gmail.com>
2022-02-28 10:58:33 -0600

    feat(client):add roles,role,getRoles() inside main-nav component

--- src/client/src/app/main-nav/main-nav.component.ts
+++ src/client/src/app/main-nav/main-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { User } from '../../../../shared/models/user.model';
+import { UserService } from '../services/user.service';
 import { AppState } from '../store';
 import { logoutUser } from '../store/actions/user/user.actions';
 import { loginUserSelector } from '../store/selectors/user/user.selectors';
@@ -13,19 +14,38 @@ import { loginUserSelector } from '../store/selectors/user/user.selectors';
 export class MainNavComponent {
 
  loginUser: User | null = null;
+ roles:string[] = []
+ role:string|null=null
 
   constructor(
-    private store:Store<AppState>,
+    private store:Store<AppState>, private userService:UserService
      ) {
 
     this.store.select(loginUserSelector).
     subscribe(data => this.loginUser = data)
+    this.getRoles()
   }
 
 
   logout() {
-    
+
     this.store.dispatch(logoutUser());
-    
+
   }
+
+   getRoles(){
+     return this.userService.getLoggedInUserRoles().subscribe(data=>{
+      this.roles = data
+       this.role=this.roles[0]
+
+     } );
+
+   }
+
+
+
+
+
+
+
 }
