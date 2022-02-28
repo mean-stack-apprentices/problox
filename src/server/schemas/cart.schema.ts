e8897b47813612c37ed452c5b06679e3dc931b2b
mekdes <mbayeleyegn@alphawork.tech>
2022-02-28 10:23:37 -0800

    feat(server):add cart schema

--- src/server/schemas/cart.schema.ts
+++ src/server/schemas/cart.schema.ts
@@ -1,17 +1,14 @@
-import mongoose from 'mongoose';
-import { Cart } from '../../shared/models/cart.model.js'
+import mongoose from "mongoose";
+import { Cart } from './../../shared/models/cart.model.js';
 
 const { Schema, model } = mongoose
 
-const cartSchema = new mongoose.Schema<Cart>({
+const cartSchema = new Schema<Cart>({
     user: { type: mongoose.Types.ObjectId, ref: 'User' },
     items: [{
-        game: {
-            type: mongoose.Types.ObjectId, ref: 'Game' },
-        merch: {
-            type: mongoose.Types.ObjectId, ref: 'Merch' },
+        merch: { type: mongoose.Types.ObjectId, ref: 'Merch' },
     }]
 });
 
-export const CartModel=model<Cart>('Cart',cartSchema)
 
+export const CartModel=model<Cart>('Cart',cartSchema)
