d913d3f27f31ad619592c602c31c8019353986aa
Gichelle Amon <amongichelle@gmail.com>
2022-03-07 11:04:36 -0800

    merge

--- src/client/src/app/modules/games/components/admin-games-list/admin-games-list.component.ts
+++ src/client/src/app/modules/games/components/admin-games-list/admin-games-list.component.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { loadGames } from '../../store/game.actions';
 import { gamesSelector } from '../../store/game.selectors';
 import { Game } from '../../../../../../../shared/models/game.model';
+import { MatDialog } from '@angular/material/dialog';
+import { EditGameComponent } from '../edit-game/edit-game.component';
 
 
 @Component({
@@ -18,12 +20,19 @@ export class AdminGamesListComponent implements OnInit {
     "imgUrl", "name","description", "price", "tier", "status", "update"
   ]
 
-  constructor(private store: Store) {
+  constructor(private store: Store, public matDialog: MatDialog) {
     this.store.dispatch(loadGames())
     this.games$ = this.store.select(gamesSelector)
   }
 
   ngOnInit(): void {
+  }
+
+  openDialog(): void{
+    let dialogBox = this.matDialog.open(EditGameComponent, {
+      width: "50rem",
+      height: "50rem",
+    })
   }
 
 }
